c3cffdf545b8d0fc88905b6e2e775465265dcc2d
Thosea <104597976+ItsThosea@users.noreply.github.com>
2024-09-14 21:49:49 -0400

    feat: write channel thread counter lastThreadedMessages to a file

--- src/main/java/me/thosea/developersdungeon/other/ChannelThreadCounter.java
+++ src/main/java/me/thosea/developersdungeon/other/ChannelThreadCounter.java
@@ -1,24 +1,32 @@
 package me.thosea.developersdungeon.other;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import it.unimi.dsi.fastutil.longs.Long2ObjectOpenHashMap;
 import me.thosea.developersdungeon.Main;
 import me.thosea.developersdungeon.util.Constants;
+import me.thosea.developersdungeon.util.Utils;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.entities.channel.concrete.ThreadChannel;
 import net.dv8tion.jda.api.entities.channel.concrete.ThreadChannel.AutoArchiveDuration;
 import org.jetbrains.annotations.Nullable;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayDeque;
 import java.util.Iterator;
 
 public class ChannelThreadCounter {
 	private static final Long2ObjectOpenHashMap<ChannelThreadCounter> COUNTERS = new Long2ObjectOpenHashMap<>();
-	private static final int STACK_SIZE_LIMIT = 15;
+	private static final int STACK_SIZE_LIMIT = 25;
 
 	private final String namePrefix;
 	private final boolean showAuthorName;
 	private final Message countMessage;
+
+	private final Path lastThreadedMessagesJson;
 
 	private final ArrayDeque<ThreadEntry> lastThreadedMessages = new ArrayDeque<>(STACK_SIZE_LIMIT);
 	private int count = 0;
@@ -26,6 +34,20 @@ public class ChannelThreadCounter {
 	public ChannelThreadCounter(String id, String namePrefix, boolean showAuthorName) {
 		this.namePrefix = namePrefix + " #";
 		this.showAuthorName = showAuthorName;
+		this.lastThreadedMessagesJson = Paths.get("./devdungeon_autothread_" + id + ".json");
+
+		if(Files.exists(lastThreadedMessagesJson)) {
+			try {
+				JsonArray array = Utils.GSON.fromJson(Files.readString(lastThreadedMessagesJson), JsonArray.class);
+				for(int i = 0; i < array.size(); i++) {
+					JsonObject obj = array.get(i).getAsJsonObject();
+					lastThreadedMessages.push(ThreadEntry.read(obj));
+				}
+			} catch(Exception e) {
+				System.err.println("Failed to read channel thread counter message stack from " + lastThreadedMessagesJson.toAbsolutePath());
+				e.printStackTrace();
+			}
+		}
 
 		long countMessageId = Constants.id("counts." + id + "_message");
 		long sourceChannel = Constants.id("counts." + id + "_channel");
@@ -90,20 +112,22 @@ public class ChannelThreadCounter {
 						}
 
 						this.lastThreadedMessages.push(new ThreadEntry(msg.getIdLong(), channel.getIdLong(), count));
+						this.writeLastThreadedMessages();
 					}
 				});
 	}
 
 	public void removeMessage(long messageId) {
+		boolean foundMessage = false;
+
 		synchronized(lastThreadedMessages) {
 			Iterator<ThreadEntry> iterator = lastThreadedMessages.descendingIterator();
 
-			boolean renameThreads = false;
 			while(iterator.hasNext()) {
 				ThreadEntry entry = iterator.next();
 				ThreadChannel thread = Main.guild.getThreadChannelById(entry.threadId);
 
-				if(renameThreads) {
+				if(foundMessage) {
 					if(thread != null) {
 						var newName = thread.getName().replaceFirst("#" + entry.count, "#" + --entry.count);
 						thread.getManager().setName(newName).queue();
@@ -111,10 +135,28 @@ public class ChannelThreadCounter {
 				} else if(entry.messageId == messageId) {
 					this.setCount(count - 1);
 					iterator.remove();
-					renameThreads = true;
+					foundMessage = true;
 					if(thread != null) thread.delete().queue();
 				}
 			}
+		}
+
+		if(foundMessage) {
+			writeLastThreadedMessages();
+		}
+	}
+
+	private void writeLastThreadedMessages() {
+		JsonArray array = new JsonArray(lastThreadedMessages.size());
+		for(Iterator<ThreadEntry> it = lastThreadedMessages.descendingIterator(); it.hasNext(); ) {
+			array.add(it.next().serialize());
+		}
+
+		try {
+			Files.writeString(lastThreadedMessagesJson, Utils.GSON.toJson(array));
+		} catch(Exception e) {
+			System.err.println("Failed to write channel thread counter message stack to " + lastThreadedMessagesJson.toAbsolutePath());
+			e.printStackTrace();
 		}
 	}
 
@@ -149,6 +191,22 @@ public class ChannelThreadCounter {
 			this.messageId = messageId;
 			this.threadId = threadId;
 			this.count = count;
+		}
+
+		public static ThreadEntry read(JsonObject obj) {
+			return new ThreadEntry(
+					obj.get("message_id").getAsLong(),
+					obj.get("thread_id").getAsLong(),
+					obj.get("thread_count").getAsInt()
+			);
+		}
+
+		public JsonObject serialize() {
+			JsonObject obj = new JsonObject();
+			obj.addProperty("message_id", messageId);
+			obj.addProperty("thread_id", threadId);
+			obj.addProperty("thread_count", count);
+			return obj;
 		}
 	}
 }
